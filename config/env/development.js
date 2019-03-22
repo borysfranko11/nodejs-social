a53287adfd1556444490c44e1be5bcac29a41a5a
pbelhe19 <shengnade100@gmail.com>
2019-03-22 06:59:55 -0700

    mongouri updated

--- config/env/development.js
+++ config/env/development.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* db: 'mongodb://localhost/noobjs_dev', */
-  db:'mongodb://nodejsdev:admin11$$@ds121406.mlab.com:21406/nodejs_dev',
+  db:'mongodb://heroku_rlhptp6t:46b5r5ng9qasjbsr8anvtftud8@ds121406.mlab.com:21406/heroku_rlhptp6t',
   facebook: {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
