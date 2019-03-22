2baee2eaed22da3fd6e239f6aa438b3fac69a325
pbelhe19 <shengnade100@gmail.com>
2019-03-22 08:43:15 -0700

    changed

--- config/env/development.js
+++ config/env/development.js
@@ -11,22 +11,7 @@ module.exports = {
     clientID: process.env.FACEBOOK_CLIENTID,
     clientSecret: process.env.FACEBOOK_SECRET,
     callbackURL: 'https://wsotp.herokuapp.com/auth/facebook/callback'
-  },
-  twitter: {
-    clientID: process.env.TWITTER_CLIENTID,
-    clientSecret: process.env.TWITTER_SECRET,
-    callbackURL: 'https://wsotp.herokuapp.com/auth/twitter/callback'
-  },
-  github: {
-    clientID: process.env.GITHUB_CLIENTID,
-    clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'https://wsotp.herokuapp.com/auth/github/callback'
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_CLIENTID,
-    clientSecret: process.env.LINKEDIN_SECRET,
-    callbackURL: 'https://wsotp.herokuapp.com/auth/linkedin/callback'
-  },
+  },  
   google: {
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRET,
